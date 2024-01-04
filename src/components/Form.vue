f731c67f4092534046f308c4bb35261476a4fd86
Diana Zavala <diana_zavalaa@hotmail.com>
2024-01-03 23:00:29 -0600

    Agregando validaciones con VeeValidate

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -2,52 +2,68 @@
   <form class="w-full max-w-lg" @submit.prevent="getFormData">
     <span v-for="error in errors">{{ error }}</span>
     <div class="flex flex-wrap -mx-3 mb-6">
-      <div class="w-full px-3 mb-6 md:mb-0">
-        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-          Nombre (s)*
-        </label>
-        <input
-          class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-          name="name" id="grid-first-name" type="text" placeholder="Nombre" v-model="newDataEmployee.name">
-        <p v-if="errors.name" class="text-red-500 text-xs italic">Por favor llene el campo.</p>
-      </div>
-      <div class="w-full px-3 mb-6 md:mb-0">
-        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-          Apellido paterno*
-        </label>
-        <input name="firstLastname"
-          class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-          id="grid-first-name" type="text" placeholder="Apellido paterno" v-model="newDataEmployee.firstLastname">
-        <p v-if="errors.firstLastname" class="text-red-500 text-xs italic">Por favor llene el campo.</p>
-      </div>
-      <div class="w-full px-3 mb-6 md:mb-0">
-        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-          Apellido materno*
-        </label>
-        <input name="secondLastname"
-          class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-          id="grid-first-name" type="text" min="18" placeholder="Apellido materno"
-          v-model="newDataEmployee.secondLastname">
-        <p v-if="errors.secondLastname" class="text-red-500 text-xs italic">Por favor llene el campo.</p>
-      </div>
-      <div class="w-full px-3 mb-6 md:mb-0">
-        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-          Edad*
-        </label>
-        <input name="age"
-          class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-          id="grid-first-name" type="number" placeholder="Edad" min="18" v-model="newDataEmployee.age">
-        <p v-if="errors.age" class="text-red-500 text-xs italic">Por favor llene el campo.</p>
-      </div>
-      <div class="w-full px-3 mb-6 md:mb-0">
-        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-          Salario*
-        </label>
-        <input name="salary"
-          class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-          id="grid-first-name" type="number" placeholder="Salario" v-model="newDataEmployee.salary">
-        <p v-if="errors.salary" class="text-red-500 text-xs italic">Por favor llene el campo.</p>
-      </div>
+      <validation-provider name="name" rules="required" v-slot="{ invalid, errors }">
+        <div class="w-full px-3 mb-6 md:mb-0">
+          <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+            Nombre (s)*
+          </label>
+          <input v-bind:class="{ 'border-red-500': invalid }"
+            class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+            name="name" id="grid-first-name" type="text" placeholder="Nombre" v-model="newDataEmployee.name">
+          <p v-for="error in errors" class="text-red-500 text-xs italic">{{ error }}</p>
+        </div>
+      </validation-provider>
+
+      <validation-provider name="firstLastname" rules="required" v-slot="{ invalid, errors }">
+        <div class="w-full px-3 mb-6 md:mb-0">
+          <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+            Apellido paterno*
+          </label>
+          <input v-bind:class="{ 'border-red-500': invalid }" name="firstLastname"
+            class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+            id="grid-first-name" type="text" placeholder="Apellido paterno" v-model="newDataEmployee.firstLastname">
+          <p v-for="error in errors" class="text-red-500 text-xs italic">{{ error }}</p>
+        </div>
+      </validation-provider>
+
+
+      <validation-provider name="secondLastname" rules="required" v-slot="{ invalid, errors }">
+        <div class="w-full px-3 mb-6 md:mb-0">
+          <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+            Apellido materno*
+          </label>
+          <input v-bind:class="{ 'border-red-500': invalid }" name="secondLastname"
+            class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+            id="grid-first-name" type="text" min="18" placeholder="Apellido materno"
+            v-model="newDataEmployee.secondLastname">
+          <p v-for="error in errors" class="text-red-500 text-xs italic">{{ error }}</p>
+        </div>
+      </validation-provider>
+
+
+      <validation-provider name="age" rules="minmax:18,70" v-slot="{ invalid, errors }">
+        <div class="w-full px-3 mb-6 md:mb-0">
+          <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+            Edad*
+          </label>
+          <input v-bind:class="{ 'border-red-500': invalid }" name="age"
+            class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+            id="grid-first-name" type="number" placeholder="Edad" min="18" v-model="newDataEmployee.age">
+          <p v-for="error in errors" class="text-red-500 text-xs italic">{{ error }}</p>
+        </div>
+      </validation-provider>
+
+      <validation-provider name="salary" rules="required" v-slot="{ invalid, errors }">
+        <div class="w-full px-3 mb-6 md:mb-0">
+          <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+            Salario*
+          </label>
+          <input v-bind:class="{ 'border-red-500': invalid }" name="salary"
+            class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+            id="grid-first-name" type="number" placeholder="Salario" v-model="newDataEmployee.salary">
+          <p v-for="error in errors" class="text-red-500 text-xs italic">{{ error }}</p>
+        </div>
+      </validation-provider>
     </div>
     <div class="flex justify-center gap-2">
       <button
@@ -60,9 +76,37 @@
 </template>
   
 <script>
+import { ValidationProvider } from 'vee-validate';
+import { extend } from 'vee-validate';
+import { required, integer, min } from 'vee-validate/dist/rules';
+
+extend('required', {
+  ...required,
+  message: 'Se requiere llenar este campo'
+});
+
+extend('minmax', {
+  validate: (value, { min, max }) => {
+    const age = parseInt(value, 10);
+
+    if (isNaN(age)) {
+      return false;
+    }
+
+    return age >= min && age <= max;
+  },
+  params: ['min', 'max'],
+  message: 'La edad debe estar entre 18 y 70 años.',
+
+});
+
 export default {
   name: "Form",
   props: ["employee"],
+
+  components: {
+    ValidationProvider
+  },
 
   data() {
     return {
@@ -97,5 +141,9 @@ export default {
     }
   }
 }
+
+
+
+
 </script>
  
